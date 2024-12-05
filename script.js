082c8418e17f1269ba7ce416f8fa9396d33911c7
Syed Naeem Shah <naeemultimate@gmail.com>
2024-12-05 22:14:44 +0000

    Added some sound effects and animation

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const days = document.querySelectorAll('.day');
 const messageBox = document.getElementById('messageBox');
+const clickSound = new Audio('ding-101492.mp3');
 
 days.forEach(day => {
     day.addEventListener('click', () => {
+        clickSound.play();
+        //Plays the sound effect first
         const message = day.getAttribute('data-message');
         messageBox.textContent = message;
         messageBox.style.visibility = 'visible';
